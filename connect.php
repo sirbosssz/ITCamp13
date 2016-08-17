2a6c170e3cb990f884d9d713f9198e769d5d040a
Foofy <foozaasksksk@gmail.com>
2016-08-17 23:24:53 +0700

    ADD_Connect_sql

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "thaiappu_itcamp";
+$pass = "3I5vOJJhGB";
+/*
+$db_name = "____";
+*/
+
+    mysql_connect($host,$user,$pass) or die ("DIE SAS");
+    mysql_select_db($db_name) or die ("maijerDATABASEwoy");
+    mysql_query("set names utf8");
+?>
