71b5b2cfc6d5d23db34b0348c0c5fc8a1cfe1ccf
Foofy <foozaasksksk@gmail.com>
2016-08-18 11:25:31 +0700

    add_db name

---
+++ backend/check_login.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+include("connect.php");
+session_start();
+$usersame = $_POST[username];
+$password = $_POST[password]
+if($username == ""){
+    echo "โปรดกรอก Username";
+} else if ($password == ""){
+    echo "โปรดกรอก Password";
+} else {
+    $check_log = mysql_query("select * from user_itcamp where user_id ='$username' and user_pass ='$password' ");
+    $num = mysql_num_rows($check_log)
+    if $num == 1 {
+        
+    }
+}
+?>
--- backend/connect.php
+++ backend/connect.php
@@ -2,9 +2,8 @@
 $host = "localhost";
 $user = "thaiappu_itcamp";
 $pass = "3I5vOJJhGB";
-/*
-$db_name = "____";
-*/
+$db_name = "thaiapppu_itcamp";
+
 
     mysql_connect($host,$user,$pass) or die ("DIE SAS");
     mysql_select_db($db_name) or die ("maijerDATABASEwoy");
